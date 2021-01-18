f8b94325c70586ad2168058c996889c5b06e238a
musa kokcen <musa@plusminusone.co>
2021-01-18 13:37:58 +0300

    swiftUI project is created

---
+++ cryptoProfit/cryptoProfitApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoProfitApp.swift
+//  cryptoProfit
+//
+//  Created by Musa Kokcen on 18.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct cryptoProfitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
